67e9aa889fbd55fc8b9588f4e6337af44974cea2
ManojGSanningammanavar <manojsanningmmanavar@gmail.com>
2025-09-07 17:46:55 +0530

    third

--- patterns/pattern1.cpp
+++ patterns/pattern1.cpp
@@ -18,8 +18,17 @@ void print2(int n ){
         cout<<endl;
     }
 }
+
+void print3(int n){
+    for(int i =1; i<=n; i++){
+        for(int j = 1; j<=i ;j++){
+            cout << j <<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cin >> n;
-    print2(n);
+    print3(n);
 }
