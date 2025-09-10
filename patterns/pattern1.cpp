4aa0d27325c378d97a06bc658acaa49bef94f0c9
ManojGSanningammanavar <manojsanningmmanavar@gmail.com>
2025-09-10 21:30:30 +0530

    nine

--- patterns/pattern1.cpp
+++ patterns/pattern1.cpp
@@ -85,8 +85,18 @@ void print12(int n){
 }
 }
 
+void print16(int n){
+for(int i=0; i<n; i++){
+    for(char ch ='E'-i;ch<='E';ch++){
+        cout<<ch<<" ";
+
+    }
+    cout<<endl;
+}
+}
+
 int main(){
     int n;
     cin >> n;
-    print12(n);
+    print16(n);
 }
