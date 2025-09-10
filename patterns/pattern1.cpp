e52ebf41e46fc1be7f84f7462fe97c1dfb927c2b
ManojGSanningammanavar <manojsanningmmanavar@gmail.com>
2025-09-10 18:05:11 +0530

    five

--- patterns/pattern1.cpp
+++ patterns/pattern1.cpp
@@ -36,8 +36,30 @@ void print4(int n){
         cout<<endl;
 }
 }
+
+void print5(int n){
+    for (int i=1; i<=n;i++){
+        for(int j=0; j<n-i-1;j++){
+            cout<<"* ";
+        }
+    }
+}
+
+void print11(int n){
+int start = 1;
+for(int i = 0 ; i<n;i++){
+    if(i%2==0) start=1;
+    else start=0;
+    for(int j=0; j<=i; j++){
+        cout<<start;
+        start=1-start;
+    }
+    cout<<endl;
+}
+}
+
 int main(){
     int n;
     cin >> n;
-    print4(n);
+    print11(n);
 }
